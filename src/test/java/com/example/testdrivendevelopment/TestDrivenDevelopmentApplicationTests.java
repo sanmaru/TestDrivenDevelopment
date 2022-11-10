38bbec247b627457a58b01dbf1039fc1b4d3cbb3
yongkou.lee@igloo.co.kr <cloud0518!#>
2022-11-10 14:03:16 +0900

    Initial commit

---
+++ src/test/java/com/example/testdrivendevelopment/TestDrivenDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testdrivendevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDrivenDevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
